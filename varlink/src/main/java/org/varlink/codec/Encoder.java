e50687b6ae79ebb9cec34b9f37622ee582af0a77
Jens Reimann <jreimann@redhat.com>
2016-11-07 16:59:38 +0100

    Implement alignment/padding and offsets

--- varlink/src/main/java/org/varlink/codec/Encoder.java
+++ varlink/src/main/java/org/varlink/codec/Encoder.java
@@ -3,6 +3,7 @@ package org.varlink.codec;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
 import java.util.function.IntFunction;
 
 /**
@@ -15,8 +16,87 @@ import java.util.function.IntFunction;
  */
 public final class Encoder {
 
+	private static final byte BOOLEAN_FALSE = (byte) 0x00;
+	private static final byte BOOLEAN_TRUE = (byte) 0x01;
+
+	private static enum OffsetSize {
+		SIZE_1(1) {
+			@Override
+			public void encodeOffset(final ByteBuffer buffer, final Integer value) {
+				buffer.put(value.byteValue());
+			}
+
+			@Override
+			public OffsetSize ensureSize(final long size) {
+				if (size <= 0xFF) {
+					return this;
+				}
+				return SIZE_2.ensureSize(size);
+			}
+		},
+		SIZE_2(2) {
+			@Override
+			public void encodeOffset(final ByteBuffer buffer, final Integer value) {
+				buffer.putShort(value.shortValue());
+			}
+
+			@Override
+			public OffsetSize ensureSize(final long size) {
+				if (size <= 0xFFFF) {
+					return this;
+				}
+				return SIZE_4.ensureSize(size);
+			}
+		},
+		SIZE_4(4) {
+			@Override
+			public void encodeOffset(final ByteBuffer buffer, final Integer value) {
+				buffer.putInt(value.intValue());
+			}
+
+			@Override
+			public OffsetSize ensureSize(final long size) {
+				if (size <= 0xFFFFFFFF) {
+					return this;
+				}
+				return SIZE_8.ensureSize(size);
+			}
+		},
+		SIZE_8(8) {
+			@Override
+			public void encodeOffset(final ByteBuffer buffer, final Integer value) {
+				buffer.putLong(value.longValue()); // yes ... I know!
+			}
+
+			@Override
+			public OffsetSize ensureSize(final long size) {
+				if (size <= Long.MAX_VALUE) {
+					return this;
+				}
+
+				throw new IllegalStateException("Unable to record offset at that size");
+			}
+		};
+
+		public abstract void encodeOffset(ByteBuffer buffer, Integer value);
+
+		public abstract OffsetSize ensureSize(long size);
+
+		private final int size;
+
+		private OffsetSize(final int size) {
+			this.size = size;
+		}
+
+		public int getSize() {
+			return this.size;
+		}
+	}
+
 	private ByteBuffer buffer;
 	private final IntFunction<ByteBuffer> allocator;
+	private final LinkedList<Integer> offsets = new LinkedList<>();
+	private boolean lastWasVariable;
 
 	public Encoder() {
 		this.buffer = ByteBuffer.allocate(1);
@@ -24,9 +104,63 @@ public final class Encoder {
 		this.allocator = this.buffer.isDirect() ? ByteBuffer::allocateDirect : ByteBuffer::allocate;
 	}
 
+	public void encodeBoolean(final boolean value) {
+		ensureMore(1);
+		this.buffer.put(value ? BOOLEAN_TRUE : BOOLEAN_FALSE);
+		recordFixed();
+	}
+
+	public void encodeUnsignedInt8(final int value) {
+		checkRange(0, 0xFF, value);
+
+		ensureMore(1);
+		this.buffer.put((byte) (value & 0xFF));
+		recordFixed();
+	}
+
+	private void checkRange(final int min, final int max, final int value) {
+		if (value < min || value > max) {
+			throw new IllegalArgumentException(
+					String.format("Value out of range - min: %s, max: %s, value: %s", min, max, value));
+		}
+	}
+
+	public void encodeInt8(final int value) {
+		checkRange(Byte.MIN_VALUE, Byte.MAX_VALUE, value);
+		encodeInt8((byte) value);
+	}
+
+	public void encodeInt8(final byte value) {
+		ensureMore(1);
+		this.buffer.put(value);
+		recordFixed();
+	}
+
+	public void encodeInt16(final int value) {
+		checkRange(Short.MIN_VALUE, Short.MAX_VALUE, value);
+		encodeInt16((short) value);
+	}
+
+	public void encodeInt16(final short value) {
+		align(2);
+		ensureMore(2);
+		this.buffer.putShort(value);
+		recordFixed();
+
+	}
+
+	public void encodeInt32(final int value) {
+		align(4);
+		ensureMore(4);
+		this.buffer.putInt(value);
+		recordFixed();
+	}
+
 	public void encodeInt64(final long value) {
+		align(8);
 		ensureMore(8);
 		this.buffer.putLong(value);
+		recordFixed();
 	}
 
 	public void encodeString(final String value) {
@@ -34,6 +168,30 @@ public final class Encoder {
 		ensureMore(data.length + 1);
 		this.buffer.put(data);
 		this.buffer.put((byte) 0);
+
+		recordVariable();
+	}
+
+	private void recordFixed() {
+		this.lastWasVariable = false;
+	}
+
+	private void recordVariable() {
+		this.lastWasVariable = true;
+		final int pos = this.buffer.position(); // already at next position
+		this.offsets.add(pos);
+	}
+
+	/**
+	 * Align current buffer position to a multiplier of 8
+	 */
+	private void align(final int alignment) {
+		final int pos = this.buffer.position();
+		if (pos % alignment != 0) {
+			final int padding = alignment - pos % alignment;
+			ensureMore(padding);
+			this.buffer.position(this.buffer.position() + padding);
+		}
 	}
 
 	private void ensureMore(final int bytes) {
@@ -57,6 +215,32 @@ public final class Encoder {
 	}
 
 	public ByteBuffer close() {
+
+		// don't encode last offsets
+
+		if (this.lastWasVariable) {
+			this.offsets.removeLast();
+		}
+
+		// encode offsets
+
+		if (!this.offsets.isEmpty()) {
+
+			// eval offset size
+
+			OffsetSize offsetSize = OffsetSize.SIZE_1;
+
+			for (final Integer offset : this.offsets) {
+				offsetSize = offsetSize.ensureSize(offset);
+			}
+
+			final int space = this.offsets.size() * offsetSize.getSize();
+			ensureMore(space);
+			while (!this.offsets.isEmpty()) {
+				offsetSize.encodeOffset(this.buffer, this.offsets.pollLast());
+			}
+		}
+
 		final ByteBuffer result = this.buffer.asReadOnlyBuffer();
 		result.flip();
 		return result;
--- varlink/src/test/java/org/varlink/codec/CodecTest.java
+++ varlink/src/test/java/org/varlink/codec/CodecTest.java
@@ -25,6 +25,52 @@ public class CodecTest {
 		assertByteBuffer(new byte[] { 0x2a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }, result);
 	}
 
+	@Test
+	public void testStruct1() {
+		final Encoder encoder = new Encoder();
+
+		encoder.encodeString("foo");
+		encoder.encodeBoolean(true);
+		encoder.encodeInt64(42);
+
+		final ByteBuffer result = encoder.close();
+
+		assertByteBuffer(new byte[] { //
+				0x66, 0x6f, 0x6f, 0x00, // "foo"
+				0x01, // true = 0x01
+				0x00, 0x00, 0x00, // 3x padding
+				0x2a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // the answer
+				0x04 // offset
+		}, result);
+	}
+
+	@Test
+	public void testStruct2() {
+
+		final Encoder encoder = new Encoder();
+
+		encoder.encodeBoolean(true);
+		encoder.encodeString("foo");
+		encoder.encodeUnsignedInt8(42);
+		encoder.encodeString("");
+		encoder.encodeInt32(-42);
+		encoder.encodeString("foobar");
+
+		final ByteBuffer result = encoder.close();
+
+		assertByteBuffer(new byte[] { //
+				0x01, // true = 0x01
+				0x66, 0x6f, 0x6f, 0x00, // "foo"
+				0x2a, // the answer = uint8_t
+				0x00, // ""
+				0x00, // 1x padding
+				// the negative answer = int32_t
+				(byte) 0xd6, (byte) 0xff, (byte) 0xff, (byte) 0xff, //
+				0x66, 0x6f, 0x6f, 0x62, 0x61, 0x72, 0x00, // "foobar"
+				0x07, 0x05 // offset
+		}, result);
+	}
+
 	private static void assertByteBuffer(final byte[] expected, final ByteBuffer actual) {
 		final byte[] data = new byte[actual.remaining()];
 		actual.get(data);
